96fa30f49f5301d2bc1da9e45e358b0d40183e7d
Fredrik Reveny <chetic@gmail.com>
2015-01-13 13:26:54 +0000

    Acting on rising edge in button signals. Retrying connecting 20 times with 500ms sleeps to work with systemd at startup

--- src/bluezcomm.c
+++ src/bluezcomm.c
@@ -11,6 +11,7 @@
 
 static DBusConnection* bus = NULL;
 static DBusMessage* msg = NULL;
+static DBusMessage* reply = NULL;
 static DBusError error;
 
 static const int BLUEZ_MAC_IDX = 20; //Where to place device address
@@ -21,19 +22,58 @@ static char BLUEZ_MEDIA_OPATH[] =
 		"/org/bluez/hci0/dev_XX_XX_XX_XX_XX_XX/player0";
 static char BLUEZ_MEDIA_IFACE[] = "org.bluez.MediaPlayer1";
 
-int terminateOnError(const char* msg, const DBusError* error) {
+int handleDbusError(const char* msg, const DBusError* error) {
 	if (dbus_error_is_set(error)) {
-		perror("Error: ");
 		perror(msg);
 		fprintf(stderr, "DBusError.name: %s\n", error->name);
 		fprintf(stderr, "DBusError.message: %s\n", error->message);
-		/* If the program wouldn't exit because of the error, freeing the
-		 DBusError needs to be done (with dbus_error_free(error)).
-		 NOTE:
-		 dbus_error_free(error) would only free the error if it was
-		 set, so it is safe to use even when you're unsure. */
 		return 1;
 	}
+	return 0;
+}
+
+//dbus-send --system --dest=org.bluez --print-reply /org/bluez/hci0/dev_XX_XX_XX_XX_XX_XX org.freedesktop.DBus.Properties.Get string:'org.bluez.Device1' string:'Connected'
+static int bluezcomm_adapter_is_connected(dbus_bool_t* powered) {
+	const char *arg1 = "org.bluez.Device1";
+	const char *arg2 = "Connected";
+	DBusMessageIter iter, subiter;
+	int current_type = -1;
+
+	msg = dbus_message_new_method_call(BLUEZ_NAME, BLUEZ_DEVICE_OPATH,
+			"org.freedesktop.DBus.Properties", "Get");
+	if (msg == NULL) {
+		fprintf(stderr, "Error: Ran out of memory when creating a message\n");
+		return 1;
+	}
+
+	if (!dbus_message_append_args(msg,
+	DBUS_TYPE_STRING, &arg1,
+	DBUS_TYPE_STRING, &arg2,
+	DBUS_TYPE_INVALID)) {
+		fprintf(stderr, "Ran out of memory while constructing args\n");
+		return 1;
+	}
+
+	reply = dbus_connection_send_with_reply_and_block(bus, msg, -1, &error);
+
+	if (reply == NULL) {
+		handleDbusError("Error sending get property message: ", &error);
+		return 1;
+	}
+
+	dbus_message_iter_init(reply, &iter);
+
+	if ((current_type = dbus_message_iter_get_arg_type(&iter))
+			!= DBUS_TYPE_VARIANT) {
+		handleDbusError("Error getting arguments from get property response: ",
+				&error);
+		return 1;
+	}
+
+	dbus_message_iter_recurse(&iter, &subiter);
+	dbus_message_iter_get_basic(&subiter, powered);
+
+	dbus_connection_flush(bus);
 	return 0;
 }
 
@@ -76,21 +116,23 @@ static int bluezcomm_media_call(const char* method) {
 }
 
 int bluezcomm_init(char* btmac) {
+	int i, retries = 20;
+	dbus_bool_t is_connected;
+
 	printf("Connecting to Bluetooth device %s...\n", btmac);
 	if (strlen(btmac) != 17) {
 		perror("Error: Invalid MAC address length\n");
 		return 1;
 	}
-	int i;
 	for (i = 0; i < 5; i++)
-		btmac[2+3*i] = '_';
+		btmac[2 + 3 * i] = '_';
 	memcpy(BLUEZ_DEVICE_OPATH + BLUEZ_MAC_IDX, btmac, 17);
 	memcpy(BLUEZ_MEDIA_OPATH + BLUEZ_MAC_IDX, btmac, 17);
 
 	dbus_error_init(&error);
 
 	bus = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
-	if (terminateOnError("Error: Failed to open Session bus\n", &error)) {
+	if (handleDbusError("Error: Failed to open Session bus\n", &error)) {
 		return 1;
 	}
 
@@ -99,8 +141,22 @@ int bluezcomm_init(char* btmac) {
 		return 1;
 	}
 
-	if (bluezcomm_device_call("Connect")) {
-		return 1;
+	while (retries--) {
+		if (bluezcomm_device_call("Connect")) {
+			handleDbusError("Error connecting: \n", &error);
+			return 1;
+		}
+
+		if (bluezcomm_adapter_is_connected(&is_connected)) {
+			return 1;
+		}
+
+		if (is_connected) {
+			break;
+		} else {
+			printf("Waiting for device... Is connected: %d\n", is_connected);
+		}
+		usleep(500000);
 	}
 
 	return 0;
@@ -109,6 +165,7 @@ int bluezcomm_init(char* btmac) {
 int bluezcomm_close(void) {
 	printf("Disconnecting from Bluetooth device...\n");
 	if (bluezcomm_device_call("Disconnect")) {
+		handleDbusError("Error disconnecting: \n", &error);
 		return 1;
 	}
 	return 0;
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 #include "canusb.h"
 #include "bluezcomm.h"
 
@@ -14,6 +15,8 @@ void sig_handler(int signo)
 
 int main(int argc, char* argv[])
 {
+	setbuf(stdout, NULL);
+
 	if (argc < 3)
 	{
 		printf(
@@ -25,31 +28,31 @@ int main(int argc, char* argv[])
 		printf("\ncan't catch SIGINT\n");
 
 	canusb_init(argv[1]);
-	bluezcomm_init(argv[2]);
 
-	printf("Version:\t");
-	canusb_print_version();
-	printf("Serial number:\t");
-	canusb_print_serial_number();
-	printf("Status flags:\t");
-	canusb_print_status();
+	if (bluezcomm_init(argv[2])) {
+		printf("Unable to initialize Bluetooth. Shutting down...\n");
+		bluezcomm_close();
+		exit(1);
+	}
 
 	printf("Disabling timestamps... \t");
 	canusb_disable_timestamps();
-	printf("Setting acceptance code... \t");
-	canusb_send_cmd("M00001D60\r");
-	printf("Setting acceptance mask... \t");
-	canusb_send_cmd("m00001FF0\r");
 	printf("Setting CANUSB to 100kbps... \t");
 	canusb_send_cmd("S3\r");
+	//printf("Setting acceptance code... \t");
+	//canusb_send_cmd("M00001D60\r");
+	//printf("Setting acceptance mask... \t");
+	//canusb_send_cmd("m00001FF0\r");
 	printf("Opening CAN channel... \t\t");
 	canusb_send_cmd("O\r");
 
+	CANFrame lastFrame;
+	canusb_reset_frame(&lastFrame);
+
 	while (s_running)
 	{
-		int i;
+		int i, n;
 		CANFrame* frame;
-		int n;
 
 		n = canusb_poll();
 
@@ -57,26 +60,26 @@ int main(int argc, char* argv[])
 		{
 			frame = canusb_get_frame(i);
 
+			printf("Received CAN frame: %03x\n", frame->id);
+
 			if (frame->id == 0x1D6)
 			{
 				printf("Steering wheel: %02x%02x\n", frame->data[0], frame->data[1]);
 
-				if ((frame->data[0] & 0x01))
+				if (!(lastFrame.data[0] & 0x01) && (frame->data[0] & 0x01))
 				{
 					bluezcomm_media_playpause();
 				}
-				if ((frame->data[0] & 0x20))
+				if (!(lastFrame.data[0] & 0x20) && (frame->data[0] & 0x20))
 				{
 					bluezcomm_media_next();
 				}
-				if ((frame->data[0] & 0x10))
+				if (!(lastFrame.data[0] & 0x10) && (frame->data[0] & 0x10))
 				{
 					bluezcomm_media_prev();
 				}
-			}
-			else
-			{
-				printf("Received CAN frame: %03x\n", frame->id);
+
+				memcpy(&lastFrame, frame, sizeof(CANFrame));
 			}
 		}
 		canusb_reset();
