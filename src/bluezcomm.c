231112d653bdea07f209ff384a9e7c07d37e74cf
Chetic <Chetic@gmail.com>
2015-09-03 10:24:51 +0200

    Added adapter power control to bluezcomm module

--- src/bluezcomm.c
+++ src/bluezcomm.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <dbus/dbus.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 static DBusConnection* bus = NULL;
 static DBusMessage* msg = NULL;
@@ -15,12 +17,9 @@ static DBusMessage* reply = NULL;
 static DBusError error;
 
 static const int BLUEZ_MAC_IDX = 20; //Where to place device address
-static char BLUEZ_NAME[] = "org.bluez";
 static char BLUEZ_DEVICE_OPATH[] = "/org/bluez/hci0/dev_XX_XX_XX_XX_XX_XX";
-static char BLUEZ_DEVICE_IFACE[] = "org.bluez.Device1";
 static char BLUEZ_MEDIA_OPATH[] =
 		"/org/bluez/hci0/dev_XX_XX_XX_XX_XX_XX/player0";
-static char BLUEZ_MEDIA_IFACE[] = "org.bluez.MediaPlayer1";
 
 int handleDbusError(const char* msg, const DBusError* error) {
 	if (dbus_error_is_set(error)) {
@@ -33,23 +32,21 @@ int handleDbusError(const char* msg, const DBusError* error) {
 }
 
 //dbus-send --system --dest=org.bluez --print-reply /org/bluez/hci0/dev_XX_XX_XX_XX_XX_XX org.freedesktop.DBus.Properties.Get string:'org.bluez.Device1' string:'Connected'
-static int bluezcomm_adapter_is_connected(dbus_bool_t* powered) {
-	const char *arg1 = "org.bluez.Device1";
-	const char *arg2 = "Connected";
+static int bluezcomm_get_property(const char* iface, const char* opath, const char* property, dbus_bool_t *result) {
 	DBusMessageIter iter, subiter;
 	int current_type = -1;
 
-	msg = dbus_message_new_method_call(BLUEZ_NAME, BLUEZ_DEVICE_OPATH,
-			"org.freedesktop.DBus.Properties", "Get");
+	msg = dbus_message_new_method_call("org.bluez", opath,
+									   "org.freedesktop.DBus.Properties", "Get");
 	if (msg == NULL) {
 		fprintf(stderr, "Error: Ran out of memory when creating a message\n");
 		return 1;
 	}
 
 	if (!dbus_message_append_args(msg,
-	DBUS_TYPE_STRING, &arg1,
-	DBUS_TYPE_STRING, &arg2,
-	DBUS_TYPE_INVALID)) {
+								  DBUS_TYPE_STRING, &iface,
+								  DBUS_TYPE_STRING, &property,
+								  DBUS_TYPE_INVALID)) {
 		fprintf(stderr, "Ran out of memory while constructing args\n");
 		return 1;
 	}
@@ -64,22 +61,79 @@ static int bluezcomm_adapter_is_connected(dbus_bool_t* powered) {
 	dbus_message_iter_init(reply, &iter);
 
 	if ((current_type = dbus_message_iter_get_arg_type(&iter))
-			!= DBUS_TYPE_VARIANT) {
+		!= DBUS_TYPE_VARIANT) {
 		handleDbusError("Error getting arguments from get property response: ",
-				&error);
+						&error);
 		return 1;
 	}
 
 	dbus_message_iter_recurse(&iter, &subiter);
-	dbus_message_iter_get_basic(&subiter, powered);
+	dbus_message_iter_get_basic(&subiter, result);
 
 	dbus_connection_flush(bus);
 	return 0;
 }
 
+static int bluezcomm_set_property(const char* iface, const char* opath, const char* property, dbus_bool_t* value) {
+	DBusMessageIter iter, subiter;
+	int current_type = -1;
+
+	msg = dbus_message_new_method_call("org.bluez", opath,
+									   "org.freedesktop.DBus.Properties", "Get");
+	if (msg == NULL) {
+		fprintf(stderr, "Error: Ran out of memory when creating a message\n");
+		return 1;
+	}
+
+	if (!dbus_message_append_args(msg,
+								  DBUS_TYPE_STRING, &iface,
+								  DBUS_TYPE_STRING, &property,
+								  DBUS_TYPE_INVALID)) {
+		fprintf(stderr, "Ran out of memory while constructing args\n");
+		return 1;
+	}
+
+	reply = dbus_connection_send_with_reply_and_block(bus, msg, -1, &error);
+
+	if (reply == NULL) {
+		handleDbusError("Error sending get property message: ", &error);
+		return 1;
+	}
+
+/*	dbus_message_iter_init(reply, &iter);
+
+	if ((current_type = dbus_message_iter_get_arg_type(&iter))
+		!= DBUS_TYPE_VARIANT) {
+		handleDbusError("Error getting arguments from get property response: ",
+						&error);
+		return 1;
+	}*/
+
+//	dbus_message_iter_recurse(&iter, &subiter);
+//	dbus_message_iter_get_basic(&subiter, result);
+
+	dbus_connection_flush(bus);
+	return 0;
+}
+
+bool bluezcomm_device_get_connected(void) {
+	dbus_bool_t connected;
+	bluezcomm_get_property("org.bluez.Device1", BLUEZ_DEVICE_OPATH, "Connected", &connected);
+	return connected;
+}
+
+void bluezcomm_adapter_set_powered(bool value) {
+	bluezcomm_set_property("org.bluez.Adapter1", "/org/bluez/hci0", "Powered", (dbus_bool_t*)&value);
+}
+
+bool bluezcomm_adapter_get_powered(void) {
+	dbus_bool_t powered;
+	bluezcomm_get_property("org.bluez.Adapter1", "/org/bluez/hci0", "Powered", &powered);
+	return (bool)powered;
+}
+
 static int bluezcomm_device_call(const char* method) {
-	msg = dbus_message_new_method_call(BLUEZ_NAME, BLUEZ_DEVICE_OPATH,
-			BLUEZ_DEVICE_IFACE, method);
+	msg = dbus_message_new_method_call("org.bluez", BLUEZ_DEVICE_OPATH, "org.bluez.Device1", method);
 	if (msg == NULL) {
 		fprintf(stderr, "Error: Ran out of memory when creating a message\n");
 		return 1;
@@ -97,8 +151,7 @@ static int bluezcomm_device_call(const char* method) {
 }
 
 static int bluezcomm_media_call(const char* method) {
-	msg = dbus_message_new_method_call(BLUEZ_NAME, BLUEZ_MEDIA_OPATH,
-			BLUEZ_MEDIA_IFACE, method);
+	msg = dbus_message_new_method_call("org.bluez", BLUEZ_MEDIA_OPATH, "org.bluez.MediaPlayer1", method);
 	if (msg == NULL) {
 		fprintf(stderr, "Error: Ran out of memory when creating a message\n");
 		return 1;
@@ -136,7 +189,7 @@ int bluezcomm_init(char* btmac) {
 		return 1;
 	}
 
-	if (!dbus_bus_name_has_owner(bus, BLUEZ_NAME, &error)) {
+	if (!dbus_bus_name_has_owner(bus, "org.bluez", &error)) {
 		fprintf(stderr, "Error: Bluez has no owner on the bus!\n");
 		return 1;
 	}
@@ -147,11 +200,7 @@ int bluezcomm_init(char* btmac) {
 			return 1;
 		}
 
-		if (bluezcomm_adapter_is_connected(&is_connected)) {
-			return 1;
-		}
-
-		if (is_connected) {
+		if (bluezcomm_device_get_connected()) {
 			break;
 		} else {
 			printf("Waiting for device... Is connected: %d\n", is_connected);
